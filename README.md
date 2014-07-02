0fa741d0dad30465a70456afbe6c9b198d9a0fc2
neko <techno.cat.miau@gmail.com>
2014-07-02 16:56:50 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Imager::LineTrace - It's new $module
+
+# SYNOPSIS
+
+    use Imager::LineTrace;
+
+# DESCRIPTION
+
+Imager::LineTrace is ...
+
+# LICENSE
+
+Copyright (C) neko.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+neko <techno.cat.miau@gmail.com>
---
+++ lib/Imager/LineTrace.pm
@@ -0,0 +1,39 @@
+package Imager::LineTrace;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Imager::LineTrace - It's new $module
+
+=head1 SYNOPSIS
+
+    use Imager::LineTrace;
+
+=head1 DESCRIPTION
+
+Imager::LineTrace is ...
+
+=head1 LICENSE
+
+Copyright (C) neko.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+neko E<lt>techno.cat.miau@gmail.comE<gt>
+
+=cut
+
