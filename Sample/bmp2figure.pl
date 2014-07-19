c2dfffe8f750e82213fd502b8900d6cbc5dc1faf
neko <techno.cat.miau@gmail.com>
2014-07-19 17:12:37 +0900

    出力整形

--- Sample/bmp2figure.pl
+++ Sample/bmp2figure.pl
@@ -15,7 +15,7 @@ my $figures_ref = Imager::LineTrace::trace( file => $ARGV[0] );
 my $i = 0;
 foreach my $figure (@{$figures_ref}) {
     print "-------- [", $i++, "] --------", "\n";
-    print "type        :", $figure->{type}, "\n";
+    print "type        : ", $figure->{type}, "\n";
     print "trace_value : ", sprintf("0x%06X", $figure->{value}), "\n";
     print "is_closed   : ", $figure->{is_closed}, "\n";
     foreach my $p (@{$figure->{points}}) {
--- lib/Imager/LineTrace.pm
+++ lib/Imager/LineTrace.pm
@@ -105,7 +105,7 @@ Imager::LineTrace - Line tracer
     my $i = 0;
     foreach my $figure (@{$figures_ref}) {
         print "-------- [", $i++, "] --------", "\n";
-        print "type        :", $figure->{type}, "\n";
+        print "type        : ", $figure->{type}, "\n";
         print "trace_value : ", sprintf("0x%06X", $figure->{value}), "\n";
         print "is_close: ", $figure->{is_closed}, "\n";
         foreach my $p (@{$figure->{points}}) {
